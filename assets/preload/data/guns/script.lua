9248d0664178b5018601a5eb87f91e45f8d997fc
ARAS AKGÜL <94735664+aras123457@users.noreply.github.com>
2022-04-01 23:35:19 +0300

    Add files via upload

---
+++ assets/preload/data/guns/script.lua
@@ -0,0 +1,10 @@
+local allowCountdown = true
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
+		setProperty('inCutscene', false);
+		startVideo('gunsCutscene');
+		allowCountdown = false;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
